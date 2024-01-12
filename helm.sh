242151b73f56556d78cb3d4e9456891b6c2b5e7f
lacinazina <zinalacina@gmail.com>
2024-01-12 11:36:26 -0600

    update data

--- helm.sh
+++ helm.sh
@@ -1,2 +1,3 @@
-helm upgrade --install appsets-mesh nailabx/appsets-mesh --version 0.1.4  --namespace test 
-helm upgrade --install nginx-argocd nailabx/nginx-argocd --version 0.1.1  --namespace test 
+helm upgrade --install argocd argo-cd --version 5.52.1  --namespace argocd --create-namespace
+helm upgrade --install appsets-mesh nailabx/appsets-mesh --version 0.1.4  --namespace test --create-namespace
+helm upgrade --install nginx-argocd nailabx/nginx-argocd --version 0.1.1  --namespace test --create-namespace
